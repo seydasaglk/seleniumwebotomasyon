3740901b34d9ae09a2e126f227cceb2dc4151fcc
Şerife Şeyda Sağlık <81486088+seydasaglk@users.noreply.github.com>
2021-09-24 23:23:24 +0300

    Add files via upload

---
+++ GittiGidiyorPack.java
@@ -0,0 +1,26 @@
+package GittiGidiyorPack;
+
+public class LoginPageTest {
+
+    protected String link = "https://www.gittigidiyor.com/";
+    protected String login ="//*[@class='gekhq4-4 egoSnI']";
+    protected String EmailId="L-UserNameField";
+    protected String EmailData="test1otomasyon@gmail.com";
+    protected String PasswordId="L-PasswordField";
+    protected String PasswordData="Otomasyon1";
+    protected String LoginId="gg-login-enter";
+    protected String SearchXpath="/html/body/div[1]/header/div[3]/div/div[2]/form/div/div[1]/div[2]/input";
+    protected String SearchData="bilgisayar";
+    protected String FindButtonXpath="//*[@data-cy=\"search-find-button\"]";
+    protected StringSecondPage="//a[@href=\\\"https://www.gittigidiyor.com/arama/?k=bilgisayar&sf=2\"\\\"][text()='2']";
+    protected String SelectProduct="//a[class=\"sc-1o6eogh-0     i3yadh-0 gmgqoT sc-1n49x8z-9 jVhkOY\"]/div/p";
+    protected String CloseAds="//img[@class='wis-clsbtn-86912']";
+    protected String ProductPrice="//div[@id='sp-price-discountPrice']";
+    protected String AddToBasketId="add-to-basket";
+    protected String MyBasket="//*[@class='basket-title']";
+    protected String NewProductPrice="//*[@class='new-price']";
+    protected String    NumberOfProduct="//select[@class='amount']";
+    protected String SelectedNumberOfProduct="select[@class='amount']/option[2]";
+    protected String DeleteProduct="btn-delete btn-update-item";
+
+}
